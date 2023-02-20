b392ecb02bd9efd77af45b148bacf61f04f115de
annielam0623 <annzhouyy@gmail.com>
2023-02-19 20:38:51 -0800

    Add Edit and Delete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,75 +4,82 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.render('index',
-    {
-      breads: Bread,
-      title: 'Index Page'
-     
-    }
-  )
-  // res.send(Bread)
-})
-
-// NEW --this section must above SHOW, otherwise will show 404, bcz the [new] is not is the bread_data
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
-
-
-// SHOW route
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+  Bread.find()
+    .then(foundBreads => {
+      res.render('index', {
+          breads: foundBreads,
+          title: 'Index Page'
+        })
+      })
     })
-  } else {
-    res.send('404')
-  }
-})
 
-// CREATE POST
-breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-  }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = 'true'
-  } else {
-    req.body.hasGluten = 'false'
-  }
-  Bread.push(req.body)
-  res.redirect('/breads')
-})
-
-
-//CREATE DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
-})
-
-//CREATE UPDATE Route
-breads.put('/:arrayIndex', (req, res) => {
-  if (req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
-})
-
-//CREATE EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+  // CREATE POST
+  breads.post('/', (req, res) => {
+    if (!req.body.image) {
+      req.body.image = undefined
+    }
+    if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = 'true'
+    } else {
+      req.body.hasGluten = 'false'
+    }
+    Bread.create(req.body)
+    res.redirect('/breads')
   })
-})
+
+  // NEW --this section must above SHOW, otherwise will show 404, bcz the [new] is not is the bread_data
+  breads.get('/new', (req, res) => {
+    res.render('new')
+  })
+
+
+  // SHOW route
+  breads.get('/:id', (req, res) => {
+    Bread.findById(req.params.id) 
+      .then(foundBread => {
+        res.render('show', {
+            bread: foundBread,
+            index: req.params.id
+        })
+      })
+      .catch(err => {
+        res.send('404')
+      })
+    })
 
 
 
+  //CREATE DELETE
+  breads.delete('/:id', (req, res) => {
+    Bread.findByIdAndDelete(req.params.id)
+    .then(deleteBread => {
+      res.status(303).redirect('/breads')
+    })
+  })
 
-module.exports = breads
+  //CREATE UPDATE Route
+  breads.put('/:id', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(updatedBread => {
+      console.log(updatedBread);
+      res.redirect(`/breads/${req.params.id}`)
+    })
+  })
+
+  //CREATE EDIT
+  breads.get('/:id/edit', (req, res) => {
+    Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('edit', {
+        bread: foundBread
+    })   
+    })
+  })
+
+
+  module.exports = breads
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const methodOverride = require('method-override')
-
-
 // CONFIGURATION
+
+const express = require('express')
 require('dotenv').config()
-const PORT = process.env.PORT
 const app = express()
+const PORT = process.env.PORT
+const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
+
+// DEPENDENCIES
+const methodOverride = require('method-override')
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+//MONGOOSE
+mongoose.set('strictQuery', true);
+mongoose.connect(
+  MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('connected to mongo: ', process.env.MONGO_URI)
+  }
+)
 
 
 // ROUTES
@@ -21,10 +34,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 // 404 Page
 app.get('*', (req, res) => {
   res.send('404')
@@ -36,3 +49,7 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
+
+
+
+// npm install react@^16.0.0 react-dom@^16.0.0
